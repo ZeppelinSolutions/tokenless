1bb05b58b7b6831fa39998ed30e9dbb0cfc576a1
Arseniy Klempner <adklempner@gmail.com>
2017-01-13 14:24:46 -0800

    Prototype createMarket function

--- contracts/MarketFactory.sol
+++ contracts/MarketFactory.sol
@@ -1,5 +1,20 @@
 pragma solidity ^0.4.4;
 
+import "./Market.sol";
+
 contract MarketFactory {
+
+  address[] marketContracts;
+
+  //Creates a Market contract, performs bookkeeping, transfers ownership of the contract to the sender, and returns the address
+  function createMarket(string _text) external returns (address) {
+    address marketAddress = new Market(_text);
+    marketContracts.push(marketAddress);
+
+    Market market = new Market(marketAddress);
+    market.transferOwnership(msg.sender);
+
+    return marketAddress;
+  }
 
 }
