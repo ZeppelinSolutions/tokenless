e10ec7ac413694e959aa3c120b9c1e82a9453aca
Arseniy Klempner <adklempner@gmail.com>
2017-03-10 20:05:19 +0530

    Fix test

--- test/MarketFactory.js
+++ test/MarketFactory.js
@@ -20,7 +20,7 @@ contract('MarketFactory', function(accounts) {
     console.log('1');
     let factory = await MarketFactory.new();
     console.log('1.1');
-    await factory.createMarket('This market will close after 1 block.', 10, {from: accounts[0]});
+    await factory.createMarket('This market will close after 5 blocks.', 5, {from: accounts[0]});
     console.log('1.2');
     let marketAddress = await factory.marketContracts(0);
     console.log('1.3');
@@ -33,17 +33,22 @@ contract('MarketFactory', function(accounts) {
     });
     console.log('2');
     await market.bet(false, {
-      from: accounts[2], 
+      from: accounts[2],
       value: web3.toWei('1','ether')
     });
     console.log('3');
 
 
+
+    let endDate = await market.endDate();
+
+    while(web3.eth.blockNumber < endDate) {
+      await web3.eth.sendTransaction({from: accounts[5], to: accounts[6], value: 10});
+    }
+
     let txnHash = await market.checkDate();
 
-
     txnHash = await market.chooseOutcome(true);
-
     await market.claimWinnings({from: accounts[1]});
     console.log('4');
     await market.claimWinnings({from: accounts[2]});
@@ -69,7 +74,7 @@ contract('MarketFactory', function(accounts) {
 
   it('bigger bet for winning outcome is rewarded more than smaller bet', async function() {
     let factory = await MarketFactory.new();
-    await factory.createMarket('This market will close after 1 block.', 1, {from: accounts[0]});
+    await factory.createMarket('This market will close after 5 blocks.', 5, {from: accounts[0]});
     let marketAddress = await factory.marketContracts(0);
     let market = await Market.at(marketAddress);
 
@@ -77,6 +82,12 @@ contract('MarketFactory', function(accounts) {
     await market.bet(true, {from: accounts[3], value: web3.toWei('3','ether')});
     let txnHash = await market.bet(false, {from: accounts[2], value: web3.toWei('10','ether')});
     // receiptMined = await web3.eth.getTransactionReceiptMined(txnHash);
+
+    let endDate = await market.endDate();
+
+    while(web3.eth.blockNumber < endDate) {
+      await web3.eth.sendTransaction({from: accounts[5], to: accounts[6], value: 10});
+    }
 
     txnHash = await market.checkDate();
     //receiptMined = await web3.eth.getTransactionReceiptMined(txnHash);
