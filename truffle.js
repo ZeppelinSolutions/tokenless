a159ae77969154f06baa2935a63da2e1cb78c1b6
Santiago Palladino <spalladino@gmail.com>
2017-05-21 18:11:12 -0300

    Add docker compose for development
    - Make rpc host configurable
    - Add truffle to dev dependencies

--- truffle.js
+++ truffle.js
@@ -20,7 +20,7 @@ module.exports = {
   }),
   networks: {
     development: {
-      host: "localhost",
+      host: process.env.RPC_HOST || "localhost",
       port: 8545,
       network_id: "*" // Match any network id
     }
